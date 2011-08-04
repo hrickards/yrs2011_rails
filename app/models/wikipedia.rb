4541e4813473927a51fc111b2d0d74be7fcddaf9
Harry Rickards <hrickards@gmail.com>
2011-08-04 15:23:55 +0100

    If no info on simple wikipedia, get from en wikipedia

--- app/models/wikipedia.rb
+++ app/models/wikipedia.rb
@@ -4,11 +4,18 @@ class Wikipedia < Hashie::Mash
   base_uri "http://simple.wikipedia.org/w"
   
   def self.find_overview(page_name)
+    page_name.downcase!
+    
     query = { :action => "render", :rvprop => "content", :title => page_name }
     
     headers = {"User-Agent" => "Ruby/Rails Contact hrickards@gmail.com"}
     
     data_whole = get('/index.php?', :query => query, :headers => headers).to_s
+    
+    unless data_whole["Wikipedia does not yet have an article with this name"].nil?
+      base_uri "http://en.wikipedia.org/w"
+      data_whole = get('/index.php?', :query => query, :headers => headers).to_s
+    end
     
     parsed_whole = Nokogiri::HTML(data_whole).xpath("//body").to_html
     index = parsed_whole.index("<h2")
